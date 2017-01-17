711975770afd76e9ee92c7e256ce6382ade47e60
Jens Oberender <burberius@gmail.com>
2017-01-17 22:00:24 +0100

    Initial commit of spring boot project.

---
+++ src/main/java/net/troja/eve/eveconomy/EvEconomyApplication.java
@@ -0,0 +1,12 @@
+package net.troja.eve.eveconomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvEconomyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvEconomyApplication.class, args);
+    }
+}
